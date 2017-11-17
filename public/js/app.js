9824d4a8beb66441c03fad74b2080097b0df1c06
Live Sound Systems <hillaryellis37@gmail.com>
2017-11-16 21:04:57 -0800

    populating html with poshmark items

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,24 @@
 $("#populate-items").on("click", function() {
 	$.getJSON("/api/items", function(data) {
+
+
 	  // For each one
-	  for (var i = 0; i < data.length; i++) {
+	for (var i = 0; i < data.length; i++) {
+
+			var div =  "<div data-id='"+ data[i]._id +"' class='tile'>"+
+              				"<a class='covershot-con' href='" + data[i].link + "'>"+
+                			"<img src='" + data[i].img + "'>"+
+              				"</a>"+
+              				"<div class='item-details'>"+
+                				"<p>"+ data[i].title + "</p>"+
+              				"</div>"+
+              				"<div class='save-item'>"+
+              					"<button>Save</button>"+
+              				"</div>"+
+            		  	"</div>"+
+          			  "</div>";
 	    // Display the apropos information on the page
-	    $("#items").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+	    $("#items-container").append(div);
 	  }
 	});
 });
