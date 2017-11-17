5d30da5e374eb0a074c37bd8c6b23bdbf6af2c94
Live Sound Systems <hillaryellis37@gmail.com>
2017-11-16 18:21:23 -0800

    got articles scraping

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,12 +1,55 @@
 var db = require("../models");
-
+var axios = require("axios");
+var cheerio = require("cheerio");
 module.exports = function(app) {
   
+//scrape route
+  app.get("/scrape", function(req,res){
+    axios.get("http://www.echojs.com/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
 
-  app.get("/api/articles", function(req,res){
-    console.log(req);
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article h2").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article
+        .create(result)
+        .then(function(dbArticle) {
+          // If we were able to successfully scrape and save an Article, send a message to the client
+          res.send("Scrape Complete");
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          res.json(err);
+        });
+      });
+    });
   });
 
+  app.get("/api/articles", function(req, res) {
+  // Grab every document in the Articles collection
+    db.Article
+      .find({})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
 
 
 };
